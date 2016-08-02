ff15af3dbebd6b4b36b74bfb9bd6dd7ac6ac81b2
coderkun <olli@coderkun.de>
2016-08-02 11:13:59 +0200

    fix setting environment for GSettings schema dir

--- mcg.py
+++ mcg.py
@@ -12,11 +12,6 @@ from mcg.application import Application
 
 
 if __name__ == "__main__":
-    # Set environment
-    srcdir = os.path.abspath(os.path.dirname(__file__))
-    if not os.environ.get('GSETTINGS_SCHEMA_DIR'):
-        os.environ['GSETTINGS_SCHEMA_DIR'] = os.path.join(srcdir, 'data')
-
     # Start application
     app = Application()
     exit_status = app.run(sys.argv)
--- mcg/__init__.py
+++ mcg/__init__.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
+import os
 
+
+
+
+# Set environment
+srcdir = os.path.abspath(os.path.dirname(__file__))
+datadir = os.path.join(srcdir, '..', 'data')
+
+# Set GSettings schema dir (if not set already)
+if not os.environ.get('GSETTINGS_SCHEMA_DIR'):
+    os.environ['GSETTINGS_SCHEMA_DIR'] = datadir
